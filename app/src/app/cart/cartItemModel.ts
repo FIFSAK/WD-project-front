87f874f568e1eb2d639b840079a3a82968510a01
BekarysKbtu <157203707+BekarysKbtu@users.noreply.github.com>
2024-04-24 22:55:25 +0500

    Delete app/src/app/cart directory

--- app/src/app/cart/cartItemModel.ts
+++
@@ -1,9 +0,0 @@
-import {CartClothesModel} from "./CartClothesModel";
-
-export interface CartItemModel {
-  id: number;
-  clothes: CartClothesModel;
-  size: string;
-  quantity: number;
-  user: number;
-}
