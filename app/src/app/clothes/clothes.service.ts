04b6f090f79650434d435f102e85ee6ecd943f53
Saya <s_amangeldinova@kbtu.kz>
2024-04-25 10:05:14 +0500

    error

--- app/src/app/clothes/clothes.service.ts
+++ app/src/app/clothes/clothes.service.ts
@@ -5,3 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class ClothesService {
 }
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {catchError, Observable, throwError} from 'rxjs'
+import {ClothesModel} from "./clothesModel";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class  ClothesService {
+  url = 'http://127.0.0.1:8000/api/clothes';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getClothes(filters: string): Observable<ClothesModel[]> {
+    return this.http.get<ClothesModel[]>(this.url +filters).pipe(
+      catchError(error => {
+        console.error('Error fetching clothes', error);
+        return throwError(error);
+      })
+    );
+  }
+}
